7ecfe3143982c69422ca6c88744ad194cfc6ab11
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-11-17 14:42:10 -0800

    donezo

---
+++ isogram.js
@@ -0,0 +1,27 @@
+const isIsogram = function isIsogram(text) {
+  const test = text.toUpperCase();
+  // regex what?! it works! check text for letter duplicates only
+  const pattern = /\w/;
+  // place to store letters when iterated
+  const checkerObj = {};
+  // iterate through test string
+  for (const letter of test) {
+    // create checker k,v if thing is a letter
+    if (pattern.test(letter)) {
+      // if checkerObj contains the letter then kick out FALSE
+      if (checkerObj[`${letter}`] === "seat's taken!") {
+        return false;
+      }
+      // if not then let it in to the checkerobj
+      checkerObj[`${letter}`] = "seat's taken!";
+    }
+    // log that sob to the console for debuggin'
+    console.log(checkerObj);
+  }
+
+  return true;
+};
+
+// const p = isIsogram('isogram');
+// console.log(p);
+export default isIsogram;
